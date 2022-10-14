aaa0f10a602b19afd3c2ca43b1099efe464adea1
Kiesha Prem <12025079+kieshaprem@users.noreply.github.com>
2022-10-14 13:03:23 +0100

    updates

--- compare_contact_matrices/codes/1_loadSyntheticContacts.r
+++ compare_contact_matrices/codes/1_loadSyntheticContacts.r
@@ -1,59 +1,59 @@
-load('../output/syntheticcontactmatrices2020/overall/contact_all.rdata')
-load('../output/syntheticcontactmatrices2020/overall/contact_home.rdata')
-load('../output/syntheticcontactmatrices2020/overall/contact_work.rdata')
-load('../output/syntheticcontactmatrices2020/overall/contact_school.rdata')
-load('../output/syntheticcontactmatrices2020/overall/contact_others.rdata')
+load('output/syntheticmatrices/contact_all.rdata')
+load('output/syntheticmatrices/contact_home.rdata')
+load('output/syntheticmatrices/contact_work.rdata')
+load('output/syntheticmatrices/contact_school.rdata')
+load('output/syntheticmatrices/contact_others.rdata')
 
-contacts2020 = list()
+contacts2021 = list()
 for(co in 1:length(contact_all))
 {
   ISO = names(contact_all)[co]
-  contacts2020[[ISO]]$home = contact_home[[ISO]]
-  contacts2020[[ISO]]$work = contact_work[[ISO]]
-  contacts2020[[ISO]]$school = contact_school[[ISO]]
-  contacts2020[[ISO]]$others = contact_others[[ISO]]
-  contacts2020[[ISO]]$all = contact_all[[ISO]]
+  contacts2021[[ISO]]$home = contact_home[[ISO]]
+  contacts2021[[ISO]]$work = contact_work[[ISO]]
+  contacts2021[[ISO]]$school = contact_school[[ISO]]
+  contacts2021[[ISO]]$others = contact_others[[ISO]]
+  contacts2021[[ISO]]$all = contact_all[[ISO]]
 }
 
 rm(contact_all,contact_home,contact_work,contact_school,contact_others,co,ISO)
 
-load('../output/syntheticcontactmatrices2020/urban/contact_all_urban.rdata')
-load('../output/syntheticcontactmatrices2020/urban/contact_home_urban.rdata')
-load('../output/syntheticcontactmatrices2020/urban/contact_work_urban.rdata')
-load('../output/syntheticcontactmatrices2020/urban/contact_school_urban.rdata')
-load('../output/syntheticcontactmatrices2020/urban/contact_others_urban.rdata')
+load('output/syntheticmatrices/urban/contact_all_urban.rdata')
+load('output/syntheticmatrices/urban/contact_home_urban.rdata')
+load('output/syntheticmatrices/urban/contact_work_urban.rdata')
+load('output/syntheticmatrices/urban/contact_school_urban.rdata')
+load('output/syntheticmatrices/urban/contact_others_urban.rdata')
 
-contacts2020_urban = list()
+contacts2021_urban = list()
 for(co in 1:length(contact_all))
 {
   ISO = names(contact_all)[co]
-  contacts2020_urban[[ISO]]$home = contact_home[[ISO]]
-  contacts2020_urban[[ISO]]$work = contact_work[[ISO]]
-  contacts2020_urban[[ISO]]$school = contact_school[[ISO]]
-  contacts2020_urban[[ISO]]$others = contact_others[[ISO]]
-  contacts2020_urban[[ISO]]$all = contact_all[[ISO]]
+  contacts2021_urban[[ISO]]$home = contact_home[[ISO]]
+  contacts2021_urban[[ISO]]$work = contact_work[[ISO]]
+  contacts2021_urban[[ISO]]$school = contact_school[[ISO]]
+  contacts2021_urban[[ISO]]$others = contact_others[[ISO]]
+  contacts2021_urban[[ISO]]$all = contact_all[[ISO]]
 }
 
 rm(contact_all,contact_home,contact_work,contact_school,contact_others,co,ISO)
 
-load('../output/syntheticcontactmatrices2020/rural/contact_all_rural.rdata')
-load('../output/syntheticcontactmatrices2020/rural/contact_home_rural.rdata')
-load('../output/syntheticcontactmatrices2020/rural/contact_work_rural.rdata')
-load('../output/syntheticcontactmatrices2020/rural/contact_school_rural.rdata')
-load('../output/syntheticcontactmatrices2020/rural/contact_others_rural.rdata')
+load('output/syntheticmatrices/rural/contact_all_rural.rdata')
+load('output/syntheticmatrices/rural/contact_home_rural.rdata')
+load('output/syntheticmatrices/rural/contact_work_rural.rdata')
+load('output/syntheticmatrices/rural/contact_school_rural.rdata')
+load('output/syntheticmatrices/rural/contact_others_rural.rdata')
 
-contacts2020_rural = list()
+contacts2021_rural = list()
 for(co in 1:length(contact_all))
 {
   ISO = names(contact_all)[co]
-  contacts2020_rural[[ISO]]$home = contact_home[[ISO]]
-  contacts2020_rural[[ISO]]$work = contact_work[[ISO]]
-  contacts2020_rural[[ISO]]$school = contact_school[[ISO]]
-  contacts2020_rural[[ISO]]$others = contact_others[[ISO]]
-  contacts2020_rural[[ISO]]$all = contact_all[[ISO]]
+  contacts2021_rural[[ISO]]$home = contact_home[[ISO]]
+  contacts2021_rural[[ISO]]$work = contact_work[[ISO]]
+  contacts2021_rural[[ISO]]$school = contact_school[[ISO]]
+  contacts2021_rural[[ISO]]$others = contact_others[[ISO]]
+  contacts2021_rural[[ISO]]$all = contact_all[[ISO]]
 }
 
 rm(contact_all,contact_home,contact_work,contact_school,contact_others,co,ISO)
 
-load('input/contacts.Rdata')
-save.image(file = 'input/synthetic_contacts.rdata')
+load('compare_contact_surveys/input/contacts.Rdata')
+save.image(file = 'compare_contact_surveys/input/synthetic_contacts.rdata')
