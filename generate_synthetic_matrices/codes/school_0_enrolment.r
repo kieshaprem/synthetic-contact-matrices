eea4d0217c56b80aabcbd3d0eae2913199ed470b
Kiesha Prem <12025079+kieshaprem@users.noreply.github.com>
2022-10-15 19:00:43 +0100

    updates

--- generate_synthetic_matrices/codes/school_0_enrolment.r
+++ generate_synthetic_matrices/codes/school_0_enrolment.r
@@ -71,6 +71,88 @@ for(isocheck in enrolment_levels$iso[is.na(enrolment_levels$secondary)]) enrolme
 countrycode(enrolment_levels$iso[is.na(enrolment_levels$tertiary)],origin = 'iso3c',destination = 'country.name')
 for(isocheck in enrolment_levels$iso[is.na(enrolment_levels$tertiary)]) enrolment_levels$tertiary[enrolment_levels$iso %in% isocheck] = getRegionalMean(isocheck,DATA = enrolTertiary)
 
+adjustTertiaryGrossToNet = TRUE
+if(adjustTertiaryGrossToNet)
+{
+  ### 
+  # UIS does not provide net enrolment rate by level of education for tertiary education
+  # Here we adjust the gross enrolment rate of tertiary education to project the net 
+  # enrolment rate tertiary education
+  
+  allyears_enrolpreprimarygross =  wb(indicator = "SE.PRE.ENRR", startdate = 2000, enddate = 2020)
+  allyears_enrolprimarygross =  wb(indicator = "SE.PRM.ENRR", startdate = 2000, enddate = 2020)
+  allyears_enrolsecondarygross =  wb(indicator = "SE.SEC.ENRR", startdate = 2000, enddate = 2020)
+  allyears_enroltertiarygross=  wb(indicator = "SE.TER.ENRR", startdate = 2000, enddate = 2020)
+  
+  
+  enrolPreprimary_gross  = getLatestData_indicator(ALLDATA = allyears_enrolpreprimarygross)
+  enrolPrimary_gross  = getLatestData_indicator(ALLDATA = allyears_enrolprimarygross)
+  enrolSecondary_gross  = getLatestData_indicator(ALLDATA = allyears_enrolsecondarygross)
+  enrolTertiary_gross  = getLatestData_indicator(ALLDATA = allyears_enroltertiarygross)
+  
+  enrolmentGross = list(preprimary = enrolPreprimary_gross,
+                        primary = enrolPrimary_gross,
+                        secondary = enrolSecondary_gross,
+                        tertiary = enrolTertiary_gross)
+  
+  
+  iso = as.character(enrolmentGross[[1]]$iso3c)
+  for(i in 2:length(enrolmentGross))
+  {
+    extra = as.character(enrolmentGross[[i]]$iso3c)[!(as.character(enrolmentGross[[i]]$iso3c) %in% iso)]
+    if(length(extra)>0) iso = c(iso,extra)
+    iso = sort(iso)
+  }
+  
+  iso
+  iso = iso[(iso %in% popratio$iso3c)]
+  iso = iso[(iso %in% work$iso3c)]
+  iso = iso[(iso %in% enrolment_levels$iso)]
+  
+  
+  enrolment_data = list()
+  for(j in 1:length(enrolmentGross))
+  {
+    value = array(NA,length(iso))
+    for(i in 1:length(iso))
+    {
+      index = which(enrolmentGross[[j]]$iso3c %in% iso[i])
+      if(length(index)>0) value[i] = enrolmentGross[[j]]$value[index]
+    }
+    enrolment_data[[j]] = value
+  }
+  
+  
+  enrolmentGross_levels = data.frame(iso = iso,do.call(cbind.data.frame, enrolment_data))
+  colnames(enrolmentGross_levels)[2:ncol(enrolmentGross_levels)] = names(enrolmentGross)
+  
+  
+  countrycode(enrolmentGross_levels$iso[is.na(enrolmentGross_levels$preprimary)],origin = 'iso3c',destination = 'country.name')
+  for(isocheck in enrolmentGross_levels$iso[is.na(enrolmentGross_levels$preprimary)]) enrolmentGross_levels$preprimary[enrolmentGross_levels$iso %in% isocheck] = getRegionalMean(isocheck = isocheck,DATA = enrolPreprimary_gross)
+  
+  countrycode(enrolmentGross_levels$iso[is.na(enrolmentGross_levels$primary)],origin = 'iso3c',destination = 'country.name')
+  for(isocheck in enrolmentGross_levels$iso[is.na(enrolmentGross_levels$primary)]) enrolmentGross_levels$primary[enrolmentGross_levels$iso %in% isocheck] = getRegionalMean(isocheck,DATA = enrolPrimary_gross)
+  
+  countrycode(enrolmentGross_levels$iso[is.na(enrolmentGross_levels$secondary)],origin = 'iso3c',destination = 'country.name')
+  for(isocheck in enrolmentGross_levels$iso[is.na(enrolmentGross_levels$secondary)]) enrolmentGross_levels$secondary[enrolmentGross_levels$iso %in% isocheck] = getRegionalMean(isocheck,DATA = enrolSecondary_gross)
+  
+  countrycode(enrolmentGross_levels$iso[is.na(enrolmentGross_levels$tertiary)],origin = 'iso3c',destination = 'country.name')
+  for(isocheck in enrolmentGross_levels$iso[is.na(enrolmentGross_levels$tertiary)]) enrolmentGross_levels$tertiary[enrolmentGross_levels$iso %in% isocheck] = getRegionalMean(isocheck,DATA = enrolTertiary_gross)
+  
+  
+  if(identical(as.character(enrolment_levels$iso),as.character(enrolmentGross_levels$iso)))
+  {
+    tertiary_net = array(NA, nrow(enrolment_levels))
+    reductionFactorGrossToNet = apply(cbind((enrolmentGross_levels$secondary/enrolment_levels$secondary),
+                                            (enrolmentGross_levels$primary/enrolment_levels$primary),
+                                            (enrolmentGross_levels$preprimary/enrolment_levels$preprimary)),1,max)
+    tertiary_net = (apply(cbind(enrolment_levels$tertiary,enrolmentGross_levels$tertiary),1,min))/reductionFactorGrossToNet
+    tertiary_net[tertiary_net>enrolment_levels$secondary] = enrolment_levels$secondary[tertiary_net>enrolment_levels$secondary]
+    enrolment_levels$tertiary = tertiary_net
+  }
+  
+}
+
 # allyears_enrolpreprimary=  wb(indicator = "SE.PRM.AGES", startdate = 2000, enddate = 2020)
 age_enrolprimary=  wb(indicator = "SE.PRM.AGES", startdate = 2000, enddate = 2020)
 age_enrolsecondary=  wb(indicator = "SE.SEC.AGES", startdate = 2000, enddate = 2020)
@@ -87,9 +169,9 @@ duration_secondary  = getLatestData_indicator(ALLDATA = duration_secondary)
 
 
 age_years_enrol = list(age_enrolprimary = age_enrolprimary,
-                     age_enrolsecondary = age_enrolsecondary,
-                     year_compulsory_schooling = compulsory_schooling,
-                     duration_secondary = duration_secondary)
+                       age_enrolsecondary = age_enrolsecondary,
+                       year_compulsory_schooling = compulsory_schooling,
+                       duration_secondary = duration_secondary)
 
 
 iso = as.character(age_years_enrol[[1]]$iso3c)
@@ -143,8 +225,8 @@ age_years_schooling$duration_secondary = as.integer(age_years_schooling$duration
 enrolment_singleage = data.frame(iso = iso, array(0, c(length(iso),25)))
 colnames(enrolment_singleage)[-1] = paste0('age',0:24)
 start_age_school = end_age_school = data.frame(iso = iso, array(0, c(length(iso),4)))
-colnames(start_age_school)[-1] = names(enrolmentlist)
-colnames(end_age_school)[-1] = names(enrolmentlist)
+colnames(start_age_school)[-1] = colnames(enrolment_levels)[-1]
+colnames(end_age_school)[-1] = colnames(enrolment_levels)[-1]
 
 for(i in 1:nrow(enrolment_singleage))
 {
@@ -189,7 +271,7 @@ for(i in 1:nrow(enrolment))
   enrolment[i,5] = mean(as.numeric(enrolment_singleage[i,paste0('age',15:19)]))
   enrolment[i,6] = mean(as.numeric(enrolment_singleage[i,paste0('age',20:24)]))
 }
-save(enrolment_levels,file = 'input/school/enrolment_levels.rdata')
+# save(enrolment_levels,file = 'input/school/enrolment_levels.rdata')
 save(enrolment,file = 'input/school/enrolment.rdata')
 save(end_age_school,file = 'input/school/end_age_school.rdata')
 save(start_age_school,file = 'input/school/start_age_school.rdata')
